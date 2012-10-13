02f6eb345c440e8294995d6fa339590ebc5930f1
cuzit <mtsilvey@gmail.com>
2012-10-13 17:43:25 -0400

    Starting on the main

---
+++ AsciiConvert.java
@@ -0,0 +1,23 @@
+/**************************************
+*Project 2 Main                       *
+*Garrett - CS232 - Image Manipulation *
+*Matt, Tyler, Crystal, Aaron, Jackie  *
+***************************************/
+import java.awt.*;
+import javax.swing.*;
+
+public class AsciiConvert extends JFrame {
+  public AsciiConvert() {
+    
+  }
+  
+  
+  public static void main(String]] args) {
+    AsciiConvert window = new AsciiConvert();
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.pack();
+    window.setVisible(true);  
+  }
+  
+  
+}
